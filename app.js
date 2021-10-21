b3721af3181582972a8db9c94fc9028761ab8721
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-10-21 20:38:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, '/public');
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-let rutasIndex = require('/routes/index.js')
+let rutasIndex = require('.routes/index.js')
 
-let rutasUsuarios = require('/routes/usuarios.js')
+let rutasUsuarios = require('./routes/usuarios.js')
 
-let rutasProductos = require('/routes/productos.js')
+let rutasProductos = require('./routes/productos.js')
 
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => console.log("Servidor funcionando"));
 
 app.use('/', rutasIndex);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './routes/index.js'));
+})
 
 app.use('/usuarios', rutasUsuarios);
 
