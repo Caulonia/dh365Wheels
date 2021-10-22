e1329291c170ba120aaadee46d8419b9c5f0e44f
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-10-21 21:36:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-let rutasIndex = require('./../index.js')
+let rutasIndex = require('./routes/index.js')
 
 let rutasUsuarios = require('./usuarios.js')
 
