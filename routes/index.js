f0b1b3490cbb3338d609f3ed7b73602c58b4952a
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-10-21 21:41:44 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const indexController = require('./controllers/index_controller.js');
+const indexController = require('../controllers/index_controller.js');
 
 let router = express.Router();
 
