88d24cde549caaedfe6043350b8642715f61c801
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-10-21 21:43:55 -0300

    Update productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const productsController = require('./products_controller');
+const productsController = require('../controllers/products_controller');
 
 let router = express.Router();
 
