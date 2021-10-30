987e9b602f3801a970848d6865d3df3a7786450a
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-10-30 11:24:02 -0300

    Update index_controller.js

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -1,7 +1,8 @@
 const indexController = {
     index: (req, res) =>{
-        res.render('index.ejs')
-    }
+        res.render('main/index.ejs')
+    },
+   
 }
 
 module.exports = indexController;
